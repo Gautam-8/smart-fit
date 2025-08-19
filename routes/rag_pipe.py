25b48876bbcdf3e1df30efc62d162511538ca2ee
Gautam H <hgautambpet@gmail.com>
2025-08-19 18:28:13 +0530

    embeddings

--- routes/rag_pipe.py
+++ routes/rag_pipe.py
@@ -9,18 +9,18 @@ from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 
 load_dotenv()
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 
 
 class Rag_pipe:
     def __init__(self):
         self.textsplitter = RecursiveCharacterTextSplitter( chunk_size=500, chunk_overlap=100)
-        self.embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
         self.vector_db = None
 
     def uploadfile(self, file: UploadFile):
         docs = PyMuPDFLoader(file).load()
         chunks = self.textsplitter.split_documents(docs)
-        self.vector_db = FAISS.from_documents(chunks, self.embeddings)
+        self.vector_db = FAISS.from_documents(chunks, embeddings)
 
     def retrieve(self, query: str):
         llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
